d486181b091f9a402142c0ff05ab75b239dbafd8
Agustín Sanchez <sanchezfabrega@gmail.com>
2017-10-28 09:32:19 +0200

    Fix database

--- config/database.php
+++ config/database.php
@@ -68,7 +68,7 @@ return [
 
         'mysql_tests' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE_TEST', 'foro_tests'),
             'username' => env('MYSQL_USER', env('DB_USERNAME', 'forge')),
